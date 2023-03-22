8f099b08ea90a890821b872f294622608ebc0ba5
Aloye Oshotse <ajo24@duke.edu>
2023-03-22 19:58:10 -0400

    fixed the concentration so that it can get multiple digits; used pd.concat instead of df.append

--- OMR_Fish.py
+++ OMR_Fish.py
@@ -6,30 +6,31 @@ Created on 3/21/23
 
 import pandas as pd
 
+
 class omrFish:
     # TODO: figure out how to make this work with actual data (how to load in id and all other relevant info).
     # could use the path to parse out the id, age, and concentration (refer to fishflux)
     def __init__(self, fish_path, id, active):
-        self.stimuli = "stim"
         self.active = active
         self.fish_path = fish_path
         self.id = id
         self.load_fish_details()
 
-    "EK_6dpf_DOI_0ugml_20230206"
-
-    "[EK, 6dpf, DOI, 0ugml, 20230206]"
-
     def load_fish_details(self):
         string_path = str(self.fish_path)
         string_list = string_path.split("_")
-        self.concentration = int(string_list[-2][0])
+        conc = string_list[-2]
+        index = conc.rfind("ugml")
+        self.concentration = int(conc[:index])
         self.age = int(string_list[1][0])
+        if (self.concentration == 0):
+            self.stimuli = "eggH20"
+        else:
+            self.stimuli = "DOI"
 
     def load_fish_to_dataframe(self, df_file):
         # load in the dataframe
         df = pd.read_csv(df_file)
-
         fish_dict = {
             'id': self.id,
             'age': self.age,
@@ -37,5 +38,8 @@ class omrFish:
             'stimuli': self.stimuli,
             'active': self.active
         }
-        df = df.append(fish_dict, ignore_index=True)
+        # converting the dictionary into a pandas dataframe
+        new_data = pd.DataFrame(fish_dict, index=[0])
+        df = pd.concat([df, new_data], axis=0)
         df.to_csv(df_file, index=False)
+
